dce6189bf8065d2add4cadcc0b933bf5e9c80d59
Jim Lambert <jim_lambert@bose.com>
2019-02-06 08:29:43 -0500

    better JSON output that includes the idea of "header" level info

--- logBuffer.go
+++ logBuffer.go
@@ -2,21 +2,40 @@ package ginlogrus
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"strings"
 )
 
 // StringWriter - implement io.Writer inferface to append to a string
-type logBuffer struct {
-	Buff strings.Builder
+type LogBuffer struct {
+	Buff      strings.Builder
+	Header    map[string]interface{}
+	AddBanner bool
 }
 
 // Write - simply append to the strings.Buffer but add a comma too
-func (b *logBuffer) Write(data []byte) (n int, err error) {
+func (b *LogBuffer) Write(data []byte) (n int, err error) {
 	newData := bytes.TrimSuffix(data, []byte("\n"))
 	return b.Buff.Write(append(newData, []byte(",")...))
 }
 
 // String - output the strings.Builder as one aggregated JSON object
-func (b *logBuffer) String() string {
-	return "{\"entries\":[" + strings.TrimSuffix(b.Buff.String(), ",") + "]}\n"
+func (b *LogBuffer) String() string {
+	var str strings.Builder
+	str.WriteString("{")
+	if b.Header != nil {
+		hdr, err := json.Marshal(b.Header)
+		if err != nil {
+			fmt.Println("Error encoding logBuffer JSON")
+		}
+		str.Write(hdr[1 : len(hdr)-1])
+		str.WriteString(",")
+	}
+	str.WriteString("\"entries\":[" + strings.TrimSuffix(b.Buff.String(), ",") + "]")
+	if b.AddBanner {
+		str.WriteString(",\"banner\":\"[GIN] --------------------------------------------------------------- GinLogrusWithTracing ----------------------------------------------------------------\"")
+	}
+	str.WriteString("}\n")
+	return str.String()
 }
