a2152740dce969547a6b3b808ae9fde91f47e17e
Jim Lambert <jim_lambert@bose.com>
2019-02-21 17:00:08 -0500

    refactor options to separate files

--- logBuffer.go
+++ logBuffer.go
@@ -10,33 +10,6 @@ import (
 	"github.com/mitchellh/copystructure"
 )
 
-// LogBufferOption - define options for LogBuffer
-type LogBufferOption func(*logBufferOptions)
-type logBufferOptions struct {
-	addBanner   bool
-	withHeaders map[string]interface{}
-}
-
-// defaultBufferOptions - some defs options to logBuffer
-var defaultLogBufferOptions = logBufferOptions{
-	addBanner:   false,
-	withHeaders: make(map[string]interface{}),
-}
-
-// WithBanner - define an Option func for passing in an optional add Banner
-func WithBanner(a bool) LogBufferOption {
-	return func(o *logBufferOptions) {
-		o.addBanner = a
-	}
-}
-
-// WithHeader - define an Option func for passing in a set of optional header
-func WithHeader(k string, v interface{}) LogBufferOption {
-	return func(o *logBufferOptions) {
-		o.withHeaders[k] = v
-	}
-}
-
 // LogBuffer - implement io.Writer inferface to append to a string
 type LogBuffer struct {
 	Buff      strings.Builder
