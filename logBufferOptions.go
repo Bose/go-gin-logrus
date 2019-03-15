ada4a35b64fa23e838d1dc3b3afa346486e16c1a
Jim Lambert <jim_lambert@bose.com>
2019-03-15 14:24:25 -0400

    support max size for the log buffer

--- logBufferOptions.go
+++ logBufferOptions.go
@@ -5,12 +5,14 @@ type LogBufferOption func(*logBufferOptions)
 type logBufferOptions struct {
 	addBanner   bool
 	withHeaders map[string]interface{}
+	maxSize     uint
 }
 
-// defaultBufferOptions - some defs options to logBuffer
-var defaultLogBufferOptions = logBufferOptions{
-	addBanner:   false,
-	withHeaders: make(map[string]interface{}),
+// DefaultLogBufferMaxSize - avg single spaced page contains 3k chars, so 100k == 33 pages which is a reasonable max
+const DefaultLogBufferMaxSize = 100000
+
+func defaultLogBufferOptions() logBufferOptions {
+	return logBufferOptions{maxSize: DefaultLogBufferMaxSize}
 }
 
 // WithBanner - define an Option func for passing in an optional add Banner
@@ -23,6 +25,14 @@ func WithBanner(a bool) LogBufferOption {
 // WithHeader - define an Option func for passing in a set of optional header
 func WithHeader(k string, v interface{}) LogBufferOption {
 	return func(o *logBufferOptions) {
+		if o.withHeaders == nil {
+			o.withHeaders = make(map[string]interface{})
+		}
 		o.withHeaders[k] = v
+	}
+}
+func WithMaxSize(s uint) LogBufferOption {
+	return func(o *logBufferOptions) {
+		o.maxSize = s
 	}
 }
