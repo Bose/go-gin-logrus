e65b723617fef0cfe3180ce6ac06c935a9e21d50
Jim Lambert <jim_lambert@bose.com>
2019-08-02 13:05:06 -0400

    support WithWriter()

--- options.go
+++ options.go
@@ -1,18 +1,25 @@
 package ginlogrus
 
-import "github.com/sirupsen/logrus"
+import (
+	"io"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
 
 // Option - define options for WithTracing()
 type Option func(*options)
 type options struct {
 	aggregateLogging bool
-	logLevel logrus.Level
+	logLevel         logrus.Level
+	writer           io.Writer
 }
 
 // defaultOptions - some defs options to NewJWTCache()
 var defaultOptions = options{
 	aggregateLogging: false,
-	logLevel: logrus.DebugLevel,
+	logLevel:         logrus.DebugLevel,
+	writer:           os.Stdout,
 }
 
 // WithAggregateLogging - define an Option func for passing in an optional aggregateLogging
@@ -26,5 +33,12 @@ func WithAggregateLogging(a bool) Option {
 func WithLogLevel(logLevel logrus.Level) Option {
 	return func(o *options) {
 		o.logLevel = logLevel
+	}
+}
+
+// WithWriter allows users to define the writer used for middlware aggregagte logging, the default writer is os.Stdout
+func WithWriter(w io.Writer) Option {
+	return func(o *options) {
+		o.writer = w
 	}
 }
